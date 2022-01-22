234d599091c017a61a0252eeb35212f45cbc4473
Austin Wise <AustinWise@gmail.com>
2022-01-22 12:09:23 -0800

    ureq: 2,129,920

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> Result<(), ureq::Error> {
+    let body: String = ureq::get("http://example.com")
+        .set("Example-Header", "header value")
+        .call()?
+        .into_string()?;
+    println!("got: {}", body);
+    Ok(())
 }
