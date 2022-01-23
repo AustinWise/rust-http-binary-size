c5b66a768546711952d66f53285e730cab6cac60
Austin Wise <AustinWise@gmail.com>
2022-01-22 19:58:51 -0800

    hyper: 1,415,680

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::Client;
+
+use hyper::body::HttpBody as _;
+use tokio::io::{stdout, AsyncWriteExt as _};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // Still inside `async fn main`...
+    let client = Client::new();
+
+    // Parse an `http::Uri`...
+    let uri = "http://httpbin.org/ip".parse()?;
+
+    // Await the response...
+    let mut resp = client.get(uri).await?;
+
+    println!("Response: {}", resp.status());
+
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+
+    Ok(())
 }
