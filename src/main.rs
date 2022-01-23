0d22aa271c4f2f2668ca68eb407da26bfb331a08
Austin Wise <AustinWise@gmail.com>
2022-01-22 19:40:25 -0800

    curl: 564,224

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{stdout, Write};
+
+use curl::easy::Easy;
+
 fn main() {
-    println!("Hello, world!");
+    let mut easy = Easy::new();
+    easy.url("https://www.rust-lang.org/").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    println!("{}", easy.response_code().unwrap());
 }
